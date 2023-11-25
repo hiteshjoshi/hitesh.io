6d021d738bad220b101565855c7994bb9681e94a
Hitesh Joshi <hi@hitesh.io>
2023-11-25 15:46:16 +0530

    npm:astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), alpinejs()]
+  integrations: [mdx(), sitemap(), tailwind(), alpinejs(), compress()]
 });
