0b25f415905ddf59e3b868dd7fc6e79a5fa3646a
Hitesh Joshi <hi@hitesh.io>
2023-11-25 15:00:55 +0530

    hello-world

---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,10 @@
+---
+title: 'Hello world'
+description: 'Hello world'
+pubDate: 'Nov 25 2023 2:57pm'
+---
+
+This is a hello world post. I accidently deleted my ghost blog on railway.app!
+Hence, I decided to write a static blog from scratch. I used astro, tailwind and some weekend motivation.
+
+It's bare minimum, but will get the job done.
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,13 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).default([]),
+  }),
 });
 
 export const collections = { blog };
