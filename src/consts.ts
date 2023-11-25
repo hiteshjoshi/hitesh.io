52934cce0feb1d8c0b99d679c8631763741ae905
Hitesh Joshi <hi@hitesh.io>
2023-11-25 14:42:05 +0530

    website finals

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "move. ~ writings by Hitesh";
+export const SITE_DESCRIPTION =
+  "Welcome to move. ,this is where I jot down my random thoughts, cool stuff I'm learning, and tales from my startup life. Always up for a good chat and a laugh. ";
