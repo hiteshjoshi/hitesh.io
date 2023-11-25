ce835dfface5c27ee3da2560a08f014a1c2f0bc3
Hitesh Joshi <hi@hitesh.io>
2023-11-25 23:28:13 +0530

    remove unwanted .netlify scripts

--- .netlify/functions-internal/chunks/generic_bd2c7037.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_b3a33f46.mjs');
-
-export { page };
--- .netlify/functions-internal/chunks/rss_0200d0b5.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/rss_7be0fd48.mjs');
-
-export { page };
