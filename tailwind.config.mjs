bbe4bf24db81bd2a0a862f6337cb0281a8baeac9
Hitesh Joshi <hi@hitesh.io>
2023-11-25 20:12:10 +0530

    remove fonts, switch to default fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,25 +4,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily: {
-				sans: [
-					'open-sans',
-					'system-ui',
-					'-apple-system',
-					'BlinkMacSystemFont',
-					'"Segoe UI"',
-					'Roboto',
-					'"Helvetica Neue"',
-					'Arial',
-					'"Noto Sans"',
-					'sans-serif',
-					'"Apple Color Emoji"',
-					'"Segoe UI Emoji"',
-					'"Segoe UI Symbol"',
-					'"Noto Color Emoji"',
-				],
-				coder: ['reross-quadratic']
-			}
+
 		},
 	},
 	plugins: [require('@tailwindcss/typography')],
