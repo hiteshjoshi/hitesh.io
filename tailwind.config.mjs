549f8e6d8fa268e45213dead547abd97b8ab3213
Hitesh Joshi <hi@hitesh.io>
2023-11-26 00:13:36 +0530

    without typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,5 @@
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-
-		},
-	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [],
 }
