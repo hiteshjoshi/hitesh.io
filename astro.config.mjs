e70372a48b8f488eaaa85c947e34bae96208c779
Hitesh Joshi <hi@hitesh.io>
2023-11-25 22:03:52 +0530

    netlify astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-
 import compress from "astro-compress";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hitesh.io',
-  integrations: [mdx(), sitemap(), tailwind(), alpinejs(), compress()]
+  integrations: [mdx(), sitemap(), tailwind(), alpinejs(), compress()],
+  output: "server",
+  adapter: netlify()
 });
