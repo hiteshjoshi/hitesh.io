d22a9338e29c722c521c10d42702ae314ec051d7
Hitesh Joshi <hi@hitesh.io>
2023-11-28 17:16:23 +0530

    remove vercel image config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,14 +9,6 @@ let config = {
   integrations: [mdx(), sitemap(), compress(), tailwind()],
   output: 'static',
   adapter: vercelStatic({
-    image: {
-      domains: ['hitesh.io'],
-    },
-    imageService: true,
-    imagesConfig: {
-      sizes: [320, 640, 1280, 1920],
-      formats: ["image/avif", "image/webp"],
-    },
     webAnalytics: {
       enabled: true,
     },
