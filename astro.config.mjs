8d5c0d20d3d7b3c76b567fd570fe7e68d636edd1
Hitesh Joshi <hi@hitesh.io>
2024-04-26 16:32:14 +0530

    new post and remove vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from '@astrojs/vercel/static';
 import compress from "astro-compress";
 let config = {
   site: 'https://hitesh.io',
-  integrations: [mdx(), sitemap(), compress(), tailwind({ applyBaseStyles: false, nesting: true, })],
-  output: 'static',
-  adapter: vercelStatic({
-    imageService: true,
-    imagesConfig: {
-      sizes: [320, 640, 1280, 1920],
-      domains: [],
-      minimumCacheTTL: 60,
-      formats: ["image/avif", "image/webp"],
-    },
-    webAnalytics: {
-      enabled: true,
-    },
-    speedInsights: {
-      enabled: true,
-    },
-  }),
+  integrations: [mdx(), sitemap(), compress(), tailwind({ applyBaseStyles: false, nesting: true, })]
 };
 
 // https://astro.build/config
