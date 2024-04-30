60f2b292019eb13db1139c0eb14da7d0ec68f48e
Hitesh Joshi <hi@hitesh.io>
2024-04-30 12:17:38 +0530

    Pretty rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,15 +1,25 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
+
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
 		title: SITE_TITLE,
+		stylesheet: '/rss/styles.xsl',
 		description: SITE_DESCRIPTION,
 		site: context.site,
+		trailingSlash: false,
 		items: posts.map((post) => ({
 			...post.data,
+			content: sanitizeHtml(parser.render(post.body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+			}),
 			link: `/${post.slug}/`,
 		})),
 	});
