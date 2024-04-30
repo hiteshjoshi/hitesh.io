cd011c6d3eda864d79842e63699f4a0f4072735b
Hitesh Joshi <hi@hitesh.io>
2024-04-30 12:58:43 +0530

    gradient

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,5 +2,24 @@
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			animation: {
+				text: 'text 5s ease infinite',
+			},
+			keyframes: {
+				text: {
+					'0%, 100%': {
+						'background-size': '200% 200%',
+						'background-position': 'left center',
+					},
+					'50%': {
+						'background-size': '200% 200%',
+						'background-position': 'right center',
+					},
+				},
+			},
+		},
+	},
 	plugins: [require('@tailwindcss/typography')],
 }
